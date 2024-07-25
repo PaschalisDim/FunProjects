c4250577fd69354858c99baff3c856d31bd0d07c
Paschalis <48329279+PaschalisDim@users.noreply.github.com>
2024-07-25 16:56:35 +0200

    Add files via upload

---
+++ openChrome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true
+do
+    open -a "Google Chrome" https://www.reddit.com/r/MemeTemplatesOfficial/comments/ipqdkm/you_got_pranked_template/
+
+    # (30 seconds)
+    sleep 30
+done
